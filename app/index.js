b81c5cdd7f258eb0dd17479e4927394083c211a9
Felipe Pontes <felipemfpontes@gmail.com>
2016-11-17 06:34:23 -0300

    Remove Raven.js dependency

--- app/index.js
+++ app/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import {render} from 'react-dom';
-import routes from './config/routes';
-import Raven from 'raven-js';
-
-const sentryKey = 'fca245742c2548fc900c19625c9c8f6c';
-const sentryApp = '95812';
-const sentryUrl = 'https://' + sentryKey + '@app.sentry.io/' + sentryApp;
-
-Raven.config(sentryUrl).install();
+import React from 'react'
+import {render} from 'react-dom'
+import routes from './config/routes'
 
 render(routes, document.getElementById('app'))
